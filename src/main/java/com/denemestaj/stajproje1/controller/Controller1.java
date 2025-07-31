091867b1729d034f8c90ceecd2f857e4bb6941bb
Sude Nur Selvi <sudenurselvi14@gmail.com>
2025-07-31 10:10:56 +0300

    Controller'a CRUD operasyonları eklendi ve test için GET metodları eklendi

--- src/main/java/com/denemestaj/stajproje1/controller/Controller1.java
+++
@@ -1,9 +0,0 @@
-package com.denemestaj.stajproje1.controller;
-
-public class Controller1 {
-    //SERİ NO'YA GÖRE YAZILACAK CRUD SİSTEMLERİ, GERCEK PROJEDE KULLANILABİLİR OLMASI GEREK
-    // CRUD operasyonlarını araştırın ve projenizde her biri için en az 1 örnek yapınız.
-    // Her biri gerçek projelerde kullanabileceğiniz işlemler olsun.
-
-
-}
